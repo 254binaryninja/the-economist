9ab35d80ac9adef7f3acf0ae8c5914774b2fcacc
254binaryninja <arnoldmusandu@gmail.com>
2025-06-14 16:53:16 +0300

    Create drizzle configuration

---
+++ drizzle.config.ts
@@ -0,0 +1,17 @@
+import dotenv from "dotenv"
+import { defineConfig } from "drizzle-kit"
+
+
+// Load environment variables from .env.local
+dotenv.config({ path: '.env.local' });
+
+export default defineConfig({
+  schema:"./database/schema/index.ts",
+  out:"./database/migrations",
+  dialect:"postgresql",
+  dbCredentials:{
+    url:process.env.DATABASE_URL!,
+    ssl: true
+  },
+  strict:true,
+})
