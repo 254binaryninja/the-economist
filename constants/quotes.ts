6a271ca632553f6bfd13708aed3bc8e21b418b87
254binaryninja <arnoldmusandu@gmail.com>
2025-06-20 23:03:49 +0300

    create constants for auth layout

---
+++ constants/quotes.ts
@@ -0,0 +1,7 @@
+export const quotes = {
+    'John Maynard Keynes (Long-Term thinker short-term pragmatist)':'In the long run we are all dead.',
+    'Adam Smith (Father of Economics)':'It is not from the benevolence of the butcher, the brewer, or the baker that we expect our dinner, but from their regard to their own interest.',
+    'Alfred Marshall (Father of Microeconomics)':'Use mathematics as a shorthand language, rather than as an engine of inquiry … Translate into English ... Then illustrate by examples ... Burn the mathematics.',
+    'Milton Friedman (Monetarist)':'There is no such thing as a free lunch.',
+    'Warren Buffett (Value Investor)':'Price is what you pay. Value is what you get.',
+};
